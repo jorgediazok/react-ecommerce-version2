9c3e465396261951778a9247a26d0e7d98ff2660
Jorge <jorgediazok@gmail.com>
2021-02-14 00:06:06 -0300

    created context

--- src/components/CheckoutPage.js
+++ src/components/CheckoutPage.js
@@ -4,7 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 import CheckoutCard from './CheckoutCard';
 import products from '../product-data';
-import Product from './Product';
 import Total from './Total';
 
 const useStyles = makeStyles((theme) => ({
